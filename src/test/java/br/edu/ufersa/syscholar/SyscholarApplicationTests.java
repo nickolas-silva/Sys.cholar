ca660a321d86acc2526f0ac1b3ccf5f38737a98a
nickolas-silva <nickolas.silva@alunos.ufersa.edu.br>
2023-02-18 15:47:15 -0300

    def pacotes

---
+++ src/test/java/br/edu/ufersa/syscholar/SyscholarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.syscholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyscholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
