c45b8d227eb29d81ed405b883a31e81860e2ce92
Arthur Peixoto <98115467+Arthurpeixoto25@users.noreply.github.com>
2023-02-25 14:35:55 -0300

    atualizando repository

--- src/main/java/br/edu/ufersa/aipapaimacetei/Domain/repository/userRepositoy.java
+++ src/main/java/br/edu/ufersa/aipapaimacetei/Domain/repository/userRepositoy.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ufersa.aipapaimacetei.Domain.entity.User;
 
 public interface userRepositoy extends JpaRepository<User, Long>{
-	
+	User findByUuid(UUID uuid);
+	User findByEmail(String email);
 }
