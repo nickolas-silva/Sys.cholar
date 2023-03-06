13c6412a7253f9a18cd5739bd216c238c267426a
jlucasbx <jlucasb16@gmail.com>
2023-03-06 20:09:32 -0300

    first commit

---
+++ src/main/java/br/edu/ufersa/sys_scholar/SysScholarApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.sys_scholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysScholarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysScholarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufersa/sys_scholar/SysScholarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.sys_scholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysScholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
