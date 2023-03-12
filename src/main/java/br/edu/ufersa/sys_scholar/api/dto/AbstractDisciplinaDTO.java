b3391c01256fdd8ea7be99b2c81c8faa8003e3b4
jlucasbx <jlucasb16@gmail.com>
2023-03-12 13:31:44 -0300

    removendo imports não usados

--- src/main/java/br/edu/ufersa/sys_scholar/api/dto/AbstractDisciplinaDTO.java
+++ src/main/java/br/edu/ufersa/sys_scholar/api/dto/AbstractDisciplinaDTO.java
@@ -1,7 +1,5 @@
 package br.edu.ufersa.sys_scholar.api.dto;
 
-import java.util.List;
-
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/br/edu/ufersa/sys_scholar/domain/entity/Endereco.java
+++ src/main/java/br/edu/ufersa/sys_scholar/domain/entity/Endereco.java
@@ -8,7 +8,6 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 
 @Entity
