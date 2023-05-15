5def83d7b1c374ba86da2c8fe4cd704898f52ecc
jlucasbx <jlucasb16@gmail.com>
2023-05-15 11:31:39 -0300

    configurando cors globalmente

--- src/main/java/br/edu/ufersa/sys_scholar/config/security/SecurityConfig.java
+++ src/main/java/br/edu/ufersa/sys_scholar/config/security/SecurityConfig.java
@@ -3,14 +3,18 @@ package br.edu.ufersa.sys_scholar.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import br.edu.ufersa.sys_scholar.config.security.filter.AuthenticationFilter;
-import br.edu.ufersa.sys_scholar.config.security.manager.CustomAuthenticationManager;
-import br.edu.ufersa.sys_scholar.config.security.filter.JWTAuthorizationFilter;
-import lombok.AllArgsConstructor;
-
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import br.edu.ufersa.sys_scholar.config.security.filter.AuthenticationFilter;
+import br.edu.ufersa.sys_scholar.config.security.filter.JWTAuthorizationFilter;
+import br.edu.ufersa.sys_scholar.config.security.manager.CustomAuthenticationManager;
+import lombok.AllArgsConstructor;
 
 @Configuration
 @AllArgsConstructor
@@ -23,10 +27,9 @@ public class SecurityConfig {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
         authenticationFilter.setFilterProcessesUrl("/login");
         http
+                .cors(Customizer.withDefaults())
                 .csrf().disable()
                 .authorizeRequests()
-                // .anyRequest().permitAll();
-
                 .antMatchers(HttpMethod.POST, SecurityConstants.ALUNO_REGISTER_PATH).permitAll()
                 .antMatchers(HttpMethod.POST, SecurityConstants.PROFESSOR_REGISTER_PATH).permitAll()
                 .antMatchers(HttpMethod.POST, SecurityConstants.DIRETOR_REGISTER_PATH).permitAll()
@@ -38,6 +41,19 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Configure the allowed origins as per your requirements
+        configuration.addAllowedMethod("*"); // Configure the allowed methods as per your requirements
+        configuration.addAllowedHeader("*"); // Configure the allowed headers as per your requirements
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
